96e5db7504d64081e197efea23e048f5c43e9347
tie <nutthawit1513@gmail.com>
2022-09-28 21:04:52 +0700

    feat: added Post, Author models
    - Added Post and Author models
    - Registered those models to admin site

--- post-app/backend/post/admin.py
+++ post-app/backend/post/admin.py
@@ -1,3 +1,6 @@
+from .models import Author, Post
+
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Post)
--- post-app/backend/post/models.py
+++ post-app/backend/post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=1000)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '{0} {1}'.format(self.first_name, self.last_name)
