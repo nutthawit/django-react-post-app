69b78f1ca107214af1be04e7f8d0c78eb35cf983
tie <nutthawit1513@gmail.com>
2022-09-29 08:54:59 +0700

    fix(backend): renamed /post path to /posts

--- post-app/backend/post/urls.py
+++ post-app/backend/post/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from .views import PostViewSet
 
 router = routers.DefaultRouter()
-router.register(r'post', PostViewSet)
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
