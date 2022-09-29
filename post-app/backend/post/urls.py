6971f98ee4bbab17187368c6399e2e1e827649cb
tie <nutthawit1513@gmail.com>
2022-09-29 08:11:13 +0700

    feat(backend): added post api
    - Created PostSerializer
    - Created PostViewSet
    - Registered PostViewSet to router

---
+++ post-app/backend/post/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r'post', PostViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
