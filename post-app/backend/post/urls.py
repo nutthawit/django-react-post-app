d9047d23411aa30bed182a9f505cafe1c1261eb1
tie <nutthawit1513@gmail.com>
2022-09-30 14:50:23 +0700

    feat(backend): added author api

--- post-app/backend/post/urls.py
+++ post-app/backend/post/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import PostViewSet
+from .views import PostViewSet, AuthorViewSet
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
+router.register(r'authors', AuthorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- post-app/backend/post/views.py
+++ post-app/backend/post/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets, permissions
 
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Author
+from .serializers import PostSerializer, AuthorSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
     permission_classes = [permissions.AllowAny]
